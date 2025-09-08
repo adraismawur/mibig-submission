ddfdef704c5bf03b634ac1a79dad7c7aa09e3152
Arjan Draisma <arjan.draisma@wur.nl>
2025-09-08 17:33:00 +0200

    add genbank accession retrieval endpoint

---
+++ api/endpoints/validations.go
@@ -0,0 +1,44 @@
+package endpoints
+
+import (
+	"github.com/adraismawur/mibig-submission/validations/genbank"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func init() {
+	RegisterEndpointGenerator(GenbankValidationEndpoint)
+}
+
+// GenbankValidationEndpoint returns the auth endpoint, which is responsible for specifically handling authentication.
+// This means acquiring a token (logging in) and refreshing a token.
+func GenbankValidationEndpoint(db *gorm.DB) Endpoint {
+	return Endpoint{
+		Routes: []Route{
+			{
+				Method: "GET",
+				Path:   "/validations/genbank/:accession",
+				Handler: func(c *gin.Context) {
+					validateGenBank(db, c)
+				},
+			},
+		},
+	}
+}
+
+func validateGenBank(db *gorm.DB, c *gin.Context) {
+	accession := c.Param("accession")
+
+	result, err := genbank.GetGenbankAccession(accession)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+	}
+
+	if result == nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Accession not found"})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"result": result})
+}
