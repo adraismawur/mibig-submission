746e7c85ca6b451e49fc6be4080b624a59035ed4
Arjan Draisma <arjan.draisma@wur.nl>
2025-11-06 17:00:37 +0100

    reduce and totally break biosynth form

--- web/submission/edit/forms/bio_synth.py
+++ web/submission/edit/forms/bio_synth.py
@@ -28,10 +28,63 @@ from submission.utils.custom_validators import (
     validate_loci,
 )
 
+biosynthetic_classes = [
+    "NRPS",
+    "terpene",
+    "PKS",
+    "ribosomal",
+    "saccharide",
+    "other",
+]
+
+biosynthetic_sub_classes = [
+    "aminocoumarin",
+    "aminoglycoside",
+    "butyrolactone",
+    "cyclitol",
+    "Diterpene",
+    "ectoine",
+    "exopolysaccharide",
+    "fatty acid",
+    "flavin",
+    "Hemiterpene",
+    "hybrid/tailoring",
+    "Iterative type I",
+    "lipopolysaccharide",
+    "Modular type I",
+    "Monoterpene",
+    "non-nrp beta-lactam",
+    "non-nrp siderophore",
+    "nucleoside",
+    "oligosaccharide",
+    "other",
+    "pbde",
+    "phenazine",
+    "phosphonate",
+    "RiPP",
+    "Sesquiterpene",
+    "Sesterterpene",
+    "shikimate-derived",
+    "Trans-AT type I",
+    "Triterpene",
+    "trna-derived",
+    "Type I",
+    "Type II aromatic",
+    "Type II arylpolyene",
+    "Type II highly reducing",
+    "Type II",
+    "Type III",
+    "Type IV",
+    "Type V",
+    "Type VI",
+    "Unknown",
+    "unmodified",
+]
+
 
 class BiosynthClassForm(Form):
-    class_ = StringField(id="class", name="class")
-    subclass = StringField()
+    class_ = SelectField(id="class", name="class", choices=biosynthetic_classes)
+    subclass = SelectField(choices=biosynthetic_sub_classes)
     cyclases = FieldList(
         StringField(),
         widget=FieldListAddBtn(
@@ -40,13 +93,30 @@ class BiosynthClassForm(Form):
     )
 
 
+module_types = [
+    "pks-modular",
+    "nrps-type1",
+    "pks-trans-at",
+    "pks-modular-starter",
+    "pks-trans-at-starter",
+]
+
+
+class ModuleLocationForm(Form):
+    from_ = IntegerField()
+    to = IntegerField()
+
+
 class BiosynthModuleForm(Form):
-    type = StringField()
-    genes = FieldList(StringField(), widget=FieldListAddBtn(label="Add gene"))
+    genes = FieldList(
+        StringField(),
+        widget=FieldListAddBtn(label="Add gene"),
+    )
+    type = SelectField(choices=module_types)
     active = BooleanField()
 
 
-class BiosynthForm(Form):
+class BiosynthSubForm(Form):
     classes = FieldList(
         FormField(BiosynthClassForm),
         min_entries=1,
@@ -57,10 +127,11 @@ class BiosynthForm(Form):
     )
 
     modules = FieldList(
-        FormField(BiosynthModuleForm), widget=FieldListAddBtn(label="Add module")
+        FormField(BiosynthModuleForm),
+        widget=FieldListAddBtn(label="Add module"),
     )
 
 
 class BioSynthForm(Form):
 
-    biosynthesis = FormField(BiosynthForm)
+    biosynthesis = FormField(BiosynthSubForm)
