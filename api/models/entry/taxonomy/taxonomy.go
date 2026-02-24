825d35511aafcbc0c2239a284dcc0e651aa2cc50
Arjan Draisma <arjan.draisma@wur.nl>
2026-02-24 17:16:06 +0100

    add missing id to model

--- api/models/entry/taxonomy/taxonomy.go
+++ api/models/entry/taxonomy/taxonomy.go
@@ -3,6 +3,7 @@ package taxonomy
 import "github.com/adraismawur/mibig-submission/models"
 
 type Taxonomy struct {
+	ID      uint64 `json:"-"`
 	EntryID uint64 `json:"-"`
 	Name    string `json:"name"`
 	TaxID   uint64 `json:"ncbiTaxId"`
