b8b3535a4e975421d21b2d94ce0545fcd090b2b4
Arjan Draisma <arjan.draisma@wur.nl>
2026-03-03 15:50:10 +0100

    check exports functionality

---
+++ api/functions/check-exports.go
@@ -0,0 +1,200 @@
+package functions
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/adraismawur/mibig-submission/config"
+	"github.com/adraismawur/mibig-submission/db"
+	"github.com/adraismawur/mibig-submission/models/entry"
+	"github.com/goccy/go-json"
+	diff2 "github.com/rogpeppe/go-internal/diff"
+	"gorm.io/gorm"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type FileMismatchError struct {
+	error
+}
+
+func listJsonFiles() ([]string, error) {
+	dataPath, err := config.GetConfig("DATA_PATH")
+
+	if err != nil {
+		slog.Error("[check-exports] Could not get env variable for data path")
+		slog.Error("[check-exports] Did not preload MIBiG files")
+		return nil, err
+	}
+
+	databaseJsonPath := filepath.Join(dataPath, "json")
+
+	dirEntries, err := os.ReadDir(databaseJsonPath)
+
+	var files []string
+
+	for _, dirEntry := range dirEntries {
+		if dirEntry.IsDir() {
+			continue
+		}
+
+		fileParts := strings.Split(dirEntry.Name(), "/")
+
+		files = append(files, fileParts[len(fileParts)-1])
+	}
+
+	return files, err
+}
+
+func checkEntryIdentity(db *gorm.DB, accession string) error {
+	dataPath, err := config.GetConfig("DATA_PATH")
+
+	if err != nil {
+		slog.Error("[check-exports] Could not get env variable for data path")
+		slog.Error("[check-exports] Did not preload MIBiG files")
+		return err
+	}
+
+	databaseJsonPath := filepath.Join(dataPath, "json")
+
+	diffPath := filepath.Join(databaseJsonPath, "diff")
+
+	err = os.MkdirAll(diffPath, os.ModePerm)
+
+	if err != nil {
+		slog.Error("[check-exports] Could not create diff folder")
+		return err
+	}
+
+	jsonPath := filepath.Join(databaseJsonPath, accession+".json")
+
+	sourceJsonBytes, err := os.ReadFile(jsonPath)
+
+	if err != nil {
+		return err
+	}
+
+	var sourceJsonBytesIndented bytes.Buffer
+
+	err = json.Indent(&sourceJsonBytesIndented, sourceJsonBytes, "", "  ")
+
+	if err != nil {
+		return err
+	}
+
+	sourceJsonBytes = sourceJsonBytesIndented.Bytes()
+
+	dbEntry, err := entry.GetEntryFromAccession(db, accession)
+
+	if err != nil {
+		return err
+	}
+
+	dbEntryBytes, err := json.MarshalIndent(dbEntry, "", "  ")
+
+	if err != nil {
+		return err
+	}
+
+	sourceJsonString := string(sourceJsonBytes)
+	dbJsonString := string(dbEntryBytes)
+
+	if sourceJsonString != dbJsonString {
+		diff := diff2.Diff("source", sourceJsonBytes, "db", dbEntryBytes)
+
+		diffFilePath := filepath.Join(diffPath, accession+".diff")
+
+		f, err := os.Create(diffFilePath)
+
+		if err != nil {
+			return err
+		}
+		if _, err = f.Write(diff); err != nil {
+			_ = f.Close()
+			return err
+		}
+
+		_ = f.Close()
+
+		return FileMismatchError{
+			errors.New("mismatch between source and db entry export"),
+		}
+	}
+
+	return nil
+}
+
+func CheckExports() {
+	slog.Info("[check-exports] Starting JSON export check")
+
+	slog.Info("[check-exports] Overriding env variables")
+	config.OverrideEnv(config.EnvDbDialect, "sqlite")
+	config.OverrideEnv(config.EnvDbPath, "file::memory:?cache=shared")
+
+	slog.Info("[check-exports] Setting up in-memory database")
+	memDb, err := db.Connect()
+
+	if err != nil {
+		slog.Error("[check-exports] Could not connect to database")
+		panic("Could not connect to database")
+	}
+
+	slog.Info("[check-exports] Downloading MIBiG database")
+	err = entry.DownloadMIBiGdatabase()
+
+	if err != nil {
+		slog.Error("[check-exports] Could not download MIBiG database")
+		panic("Could not download MIBiG database")
+	}
+
+	slog.Info("[check-exports] Loading MIBiG entries into database")
+
+	err = entry.PreloadMibigDatabase(memDb)
+
+	if err != nil {
+		slog.Error("[check-exports] Could not load MIBiG database")
+		panic("Could not load MIBiG database")
+	}
+
+	slog.Info("[check-exports] Listing existing JSON")
+	files, err := listJsonFiles()
+
+	if err != nil {
+		slog.Error("[check-exports] Could not list JSON files")
+		panic("Could not list JSON files")
+	}
+
+	numFiles := len(files)
+	slog.Info(fmt.Sprintf("[check-exports] %d files found", numFiles))
+
+	matches, mismatches, checkErrors := 0, 0, 0
+
+	for _, file := range files {
+		parts := strings.Split(file, ".")
+
+		accession := parts[0]
+		err = checkEntryIdentity(memDb, accession)
+
+		if err != nil {
+			if errors.As(err, &FileMismatchError{}) {
+				mismatches++
+				slog.Error("[check-exports] Source and DB entry mismatch", "accession", accession)
+				continue
+			}
+			slog.Error("[check-exports] Could not check entry identity", "error", err)
+			checkErrors++
+			continue
+		}
+
+		matches++
+	}
+
+	slog.Info("[check-exports] End of JSON export check")
+	slog.Info("[check-exports] Report:")
+	slog.Info(fmt.Sprintf("[check-exports] Total number of entries: %d", numFiles))
+	slog.Info(fmt.Sprintf("[check-exports] %d matches (%f%%)", matches, float64(matches*100)/float64(numFiles)))
+	slog.Info(fmt.Sprintf("[check-exports] %d mismatches (%f%%)", mismatches, float64(mismatches*100)/float64(numFiles)))
+	slog.Info(fmt.Sprintf("[check-exports] %d errors (%f%%)", checkErrors, float64(checkErrors*100)/float64(numFiles)))
+}
--- api/go.mod
+++ api/go.mod
@@ -5,11 +5,14 @@ go 1.23.0
 toolchain go1.24.1
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/beevik/guid v1.0.0
+	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-gonic/gin v1.10.1
 	github.com/go-playground/assert/v2 v2.2.0
 	github.com/goccy/go-json v0.10.5
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/kr/pretty v0.3.0
+	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.39.0
 	gorm.io/driver/postgres v1.5.11
@@ -18,19 +21,15 @@ require (
 )
 
 require (
-	github.com/beevik/guid v1.0.0 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
-	github.com/gin-contrib/cors v1.7.6 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.2 // indirect
@@ -39,9 +38,8 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
-	github.com/kr/text v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
