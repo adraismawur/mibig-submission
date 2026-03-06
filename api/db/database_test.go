803bc7487451f035348b55be1dfc131a19f97a4f
Arjan Draisma <arjan.draisma@wur.nl>
2026-03-06 14:53:13 +0100

    remove postgres test

--- api/db/database_test.go
+++ api/db/database_test.go
@@ -13,16 +13,6 @@ func TestConnectDefaults(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-func TestConnectPostgres(t *testing.T) {
-	// set environment variables
-	config.OverrideEnv(config.EnvDbDialect, "postgres")
-	config.OverrideEnv(config.EnvDbUser, "postgres")
-
-	db, _ := Connect()
-
-	assert.NotNil(t, db, "DB using postgres dialect should not be nil")
-}
-
 func TestConnectUnsupported(t *testing.T) {
 	// set environment variables
 	config.OverrideEnv(config.EnvDbDialect, "unsupported")
