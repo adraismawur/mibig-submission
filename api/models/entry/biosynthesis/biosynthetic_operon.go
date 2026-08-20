3ca7a6d78a2f3fa51b9335f501df48ddaa603bf5
Arjan Draisma <arjan.draisma@wur.nl>
2026-08-20 14:05:57 +0200

    fix operon evidence

--- api/models/entry/biosynthesis/biosynthetic_operon.go
+++ api/models/entry/biosynthesis/biosynthetic_operon.go
@@ -5,13 +5,20 @@ import (
 	"github.com/lib/pq"
 )
 
+type BiosyntheticOperonEvidence struct {
+	ID         uint64         `json:"db_id"`
+	Method     string         `json:"method"`
+	References pq.StringArray `json:"references" gorm:"type:text[]"`
+}
+
 type BiosyntheticOperon struct {
-	ID             uint64         `json:"db_id"`
-	BiosynthesisID uint64         `json:"db_biosynth_id"`
-	Genes          pq.StringArray `json:"genes" gorm:"type:text[]"`
-	Evidence       pq.StringArray `json:"evidence" gorm:"type:text[]"`
+	ID             uint64                       `json:"db_id"`
+	BiosynthesisID uint64                       `json:"db_biosynth_id"`
+	Genes          pq.StringArray               `json:"genes" gorm:"type:text[]"`
+	Evidence       []BiosyntheticOperonEvidence `json:"evidence" gorm:"many2many:biosynthetic_operon_evidences"`
 }
 
 func init() {
+	models.Models = append(models.Models, &BiosyntheticOperonEvidence{})
 	models.Models = append(models.Models, &BiosyntheticOperon{})
 }
